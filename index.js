785215cb7206c1c749cce848fc0b3cceb6354b4f
Aamir Saleem <65162736+aamirsaleem1286@users.noreply.github.com>
2024-04-15 01:38:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+const game = await inquirer.prompt([{
+        type: "number",
+        name: "input",
+        message: "please select a number between 1 till 10: "
+    }]);
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+;
+if (game.input === randomNumber) {
+    console.log(chalk.green("correct"));
+}
+else {
+    console.log(chalk.redBright(`wrong the random number is ${randomNumber}`));
+}
